c1cbef0a33705e77510ec20e6df1172bcbf23e7b
mumtazalwan <mumtazalwan@gmail.com>
2026-06-06 12:22:25 +0700

    update

--- Home.py
+++ Home.py
@@ -1,4 +1,5 @@
 import streamlit as st
+st.image("https://images.unsplash.com/photo-1579621970563-ebec7560ff3e?q=80&w=2071&auto=format&fit=crop", use_container_width=True)
 
 st.title("Loan Risk Prediction AI")
 st.caption("Machine Learning Pipeline Project")
@@ -61,52 +62,40 @@ col1, col2, col3, col4 = st.columns(4)
 with col1:
 		st.info(
 				"""
-				Dataset & EDA
-
-				Dataset Overview
-
-				Feature Understanding
-
-				Data Exploration
+				**Dataset & EDA
+				*Dataset Overview
+				*Feature Understanding
+				*Data Exploration
 				"""
 		)
 
 with col2:
 		st.info(
 				"""
-				Preprocessing
-
-				Categorical Encoding
-
-				Train-Test Split
-
-				Feature Scaling
+				**Preprocessing
+				*Categorical Encoding
+				*Train-Test Split
+				*Feature Scaling
 				"""
 		)
 
 with col3:
 		st.info(
 				"""
-				Model Training
-
-				Logistic Regression
-
-				Random Forest
-
-				Gradient Boosting
+				**Model Training
+				*Logistic Regression
+				*Random Forest
+				*Gradient Boosting
 				"""
 		)
 
 with col4:
 		st.info(
 				"""
-				Prediction
-
-				Interactive Demo
-
-				Risk Analysis
-
-				Recommendation
+				**Prediction
+				*Interactive Demo
+				*Risk Analysis
+				*Recommendation
 				"""
 		)
 
